5dfcbb5fa4c2a1351db1eb4a3b67517623a1ea12
Mohammed <mohammed.mm404@gmail.com>
2024-07-30 03:11:06 +0300

    added template

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import options
 
 
 def create_app(config_name: str) -> Flask:
-    """Create and configure the applciation"""
+    """Create and configure the application"""
     app = Flask(__name__)
     app.config.from_object(options[config_name])
+
+    @app.route('/')
+    def index():
+        return render_template('test.html')
     
     return app
